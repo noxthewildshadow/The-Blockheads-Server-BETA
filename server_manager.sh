f125b6b2a54b023b2c66a5e772c3df19e32d8236
FerTheWildShadow <noxthewildshadow@gmail.com>
2025-09-04 19:27:27 -0600

    Update server_manager.sh

--- server_manager.sh
+++ server_manager.sh
@@ -245,7 +245,7 @@ EOF
     # Start bot
     print_step "Starting server bot..."
     screen -dmS "$SCREEN_BOT" bash -c "
-        cd '$P'
+        cd '$PWD'
         echo 'Starting server bot for port $port...'
         ./server_bot.sh '$log_file' '$port'
     "
