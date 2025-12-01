29072ff6534118354157e55e1cc4c44aaac863a5
FerTheWildShadow <noxthewildshadow@gmail.com>
2025-11-30 22:40:30 -0600

    Update freight_car_patch.c

--- freight_car_patch.c
+++ freight_car_patch.c
@@ -1,72 +1,79 @@
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>     // strstr
-#include <unistd.h>     // sleep
+#include <string.h>
+#include <unistd.h>
 #include <dlfcn.h>
 #include <pthread.h>
 #include <objc/runtime.h>
-#include <objc/message.h> // objc_msgSend
+#include <objc/message.h>
 
-// Función fake para reemplazar constructores de FreightCar
+/* FREIGHT CAR EXPLOIT FIX
+   This patch intercepts the creation of FreightCar objects to prevent server crashes.
+*/
+
+// Fake constructor to replace the real FreightCar init methods
 id fakeCtor(id self, SEL _cmd, ...) {
-    printf("[freightkill] FreightCar creation blocked: %s\n", sel_getName(_cmd));
+    printf("[Anti-Exploit] Blocked malicious FreightCar creation.\n");
 
-    // setNeedsRemoved:YES
+    // 1. Force the game engine to mark the object for removal
     SEL selSetNeedsRemoved = sel_registerName("setNeedsRemoved:");
     IMP fnSetNeedsRemoved = class_getMethodImplementation(object_getClass(self), selSetNeedsRemoved);
-    ((void (*)(id, SEL, BOOL))fnSetNeedsRemoved)(self, selSetNeedsRemoved, 1);
-
-    // dealloc
-    SEL selDealloc = sel_registerName("dealloc");
-    IMP fnDealloc = class_getMethodImplementation(object_getClass(self), selDealloc);
-    ((void (*)(id, SEL))fnDealloc)(self, selDealloc);
-
-    return NULL;
-}
-
-// Thread para parchear después de que el binario cargue todas las clases
-static void *patchThread(void *arg) {
-    sleep(1); // espera a que el runtime registre las clases
-
-    printf("[freightkill] Searching for FreightCar class...\n");
-
-    int patched = 0;
-    unsigned int classCount = objc_getClassList(NULL, 0);
-    Class *classes = malloc(sizeof(Class) * classCount);
-    classCount = objc_getClassList(classes, classCount);
-
-    for (unsigned int i = 0; i < classCount; i++) {
-        const char *name = class_getName(classes[i]);
-        if (strstr(name, "FreightCar")) {
-            printf("[freightkill] Found class: %s\n", name);
-
-            SEL sel1 = sel_registerName("initWithWorld:dynamicWorld:atPosition:cache:saveDict:placedByClient:");
-            SEL sel2 = sel_registerName("initWithWorld:dynamicWorld:cache:netData:");
-            SEL sel3 = sel_registerName("initWithWorld:dynamicWorld:saveDict:chestSaveDict:cache:");
-
-            class_replaceMethod(classes[i], sel1, (IMP)fakeCtor, "v@:*");
-            class_replaceMethod(classes[i], sel2, (IMP)fakeCtor, "v@:*");
-            class_replaceMethod(classes[i], sel3, (IMP)fakeCtor, "v@:*");
-
-            patched = 1;
-            break;
-        }
+    if (fnSetNeedsRemoved) {
+        ((void (*)(id, SEL, BOOL))fnSetNeedsRemoved)(self, selSetNeedsRemoved, 1);
     }
 
-    free(classes);
-
-    if (patched)
-        printf("[freightkill] FreightCar constructors patched successfully.\n");
-    else
-        printf("[freightkill] ERROR: Could not find FreightCar class!\n");
+    // 2. Free memory immediately
+    SEL selDealloc = sel_registerName("dealloc");
+    IMP fnDealloc = class_getMethodImplementation(object_getClass(self), selDealloc);
+    if (fnDealloc) {
+        ((void (*)(id, SEL))fnDealloc)(self, selDealloc);
+    }
 
     return NULL;
 }
 
+// Thread that waits for the server to load classes, then applies the patch
+static void *patchThread(void *arg) {
+    // Wait 2 seconds for the server to initialize the ObjC runtime
+    sleep(2); 
+
+    printf("[Anti-Exploit] Searching for FreightCar class...\n");
+
+    unsigned int classCount = 0;
+    Class *classes = objc_copyClassList(&classCount);
+
+    if (classes) {
+        for (unsigned int i = 0; i < classCount; i++) {
+            const char *name = class_getName(classes[i]);
+            
+            // Look for the target class
+            if (name && strstr(name, "FreightCar")) {
+                printf("[Anti-Exploit] Patching class: %s\n", name);
+
+                // Known selectors used by the exploit
+                SEL sel1 = sel_registerName("initWithWorld:dynamicWorld:atPosition:cache:saveDict:placedByClient:");
+                SEL sel2 = sel_registerName("initWithWorld:dynamicWorld:cache:netData:");
+                SEL sel3 = sel_registerName("initWithWorld:dynamicWorld:saveDict:chestSaveDict:cache:");
+
+                // Replace methods with our fake constructor
+                if(sel1) class_replaceMethod(classes[i], sel1, (IMP)fakeCtor, "v@:*");
+                if(sel2) class_replaceMethod(classes[i], sel2, (IMP)fakeCtor, "v@:*");
+                if(sel3) class_replaceMethod(classes[i], sel3, (IMP)fakeCtor, "v@:*");
+            }
+        }
+        free(classes);
+    } else {
+        printf("[Anti-Exploit] Error: Could not retrieve class list.\n");
+    }
+
+    return NULL;
+}
+
+// Library constructor - runs automatically when loaded via LD_PRELOAD
 __attribute__((constructor))
 static void init_hook() {
-    printf("[freightkill] Initializing patch thread...\n");
+    printf("[Anti-Exploit] Initializing FreightCar protection...\n");
     pthread_t t;
     pthread_create(&t, NULL, patchThread, NULL);
 }
