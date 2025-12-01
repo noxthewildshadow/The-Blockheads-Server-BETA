c4600fe1b675448f6849f9e4fe5c90c27ab3193f
FerTheWildShadow <noxthewildshadow@gmail.com>
2025-11-30 23:12:13 -0600

    Refactor includes and improve comments in freight_car_patch.c

--- freight_car_patch.c
+++ freight_car_patch.c
@@ -1,14 +1,14 @@
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
-#include <string.h>     // strstr
-#include <unistd.h>     // sleep
+#include <string.h>
+#include <unistd.h>
 #include <dlfcn.h>
 #include <pthread.h>
 #include <objc/runtime.h>
-#include <objc/message.h> // objc_msgSend
+#include <objc/message.h>
 
-// Función fake para reemplazar constructores de FreightCar
+// Fake constructor to replace FreightCar init methods
 id fakeCtor(id self, SEL _cmd, ...) {
     printf("[Anti-Exploit] FreightCar creation blocked: %s\n", sel_getName(_cmd));
 
@@ -29,14 +29,13 @@ id fakeCtor(id self, SEL _cmd, ...) {
     return NULL;
 }
 
-// Thread para parchear después de que el binario cargue todas las clases
+// Thread to apply patch after server loads classes
 static void *patchThread(void *arg) {
-    sleep(2); // espera a que el runtime registre las clases
+    sleep(2); // wait for runtime
 
     printf("[Anti-Exploit] Searching for FreightCar class...\n");
 
     int patched = 0;
-    // USAMOS LA API ANTIGUA compatible con el servidor
     unsigned int classCount = objc_getClassList(NULL, 0);
     Class *classes = malloc(sizeof(Class) * classCount);
     if (!classes) return NULL;
