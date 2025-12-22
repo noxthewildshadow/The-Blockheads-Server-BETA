7bbfcf54614e10f67fe85e6ce26a3665458f65cb
FerTheWildShadow <noxthewildshadow@gmail.com>
2025-12-22 16:43:28 -0600

    Update freight_car_patch.c

--- patches/freight_car_patch.c
+++ patches/freight_car_patch.c
@@ -1,8 +1,6 @@
 /*
- * Freight Car Ban
- * Target Class: FreightCar (ID 206)
+ * Ban Freight Car
  */
-
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
@@ -13,11 +11,8 @@
 #include <objc/runtime.h>
 #include <objc/message.h>
 
-// --- Configuration ---
 #define TARGET_CLASS_NAME "FreightCar"
 #define DROPPED_ITEM_ID   206 
-
-// --- Selectors ---
 #define SEL_PLACE  "initWithWorld:dynamicWorld:atPosition:cache:saveDict:placedByClient:"
 #define SEL_LOAD   "initWithWorld:dynamicWorld:saveDict:chestSaveDict:cache:"
 #define SEL_NET    "initWithWorld:dynamicWorld:cache:netData:"
@@ -25,34 +20,25 @@
 #define SEL_POS    "pos"
 #define SEL_FLAG   "setNeedsRemoved:"
 
-// --- Function Prototypes ---
-typedef id (*PlaceFunc)(id, SEL, id, id, long long, id, id, id);
 typedef id (*LoadFunc)(id, SEL, id, id, id, id, id); 
-typedef id (*NetFunc)(id, SEL, id, id, id, id);
 typedef long long (*PosReturnFunc)(id, SEL);
 typedef void (*VoidFunc)(id, SEL, BOOL);
 typedef id (*SpawnFunc)(id, SEL, long long, int, int, int, id, id, BOOL, BOOL, id);
 
-// --- Global Storage ---
 static LoadFunc real_Freight_InitLoad = NULL;
 
 long long GetPosition(id obj) {
     if (!obj) return -1;
     SEL sel = sel_registerName(SEL_POS);
     IMP method = class_getMethodImplementation(object_getClass(obj), sel);
-    if (method) return ((PosReturnFunc)method)(obj, sel);
-    return -1;
+    return method ? ((PosReturnFunc)method)(obj, sel) : -1;
 }
 
 void SpawnDroppedItem(id dynWorld, long long pos) {
     if (!dynWorld || pos == -1) return;
-
     SEL sel = sel_registerName(SEL_SPAWN);
     IMP method = class_getMethodImplementation(object_getClass(dynWorld), sel);
-
-    if (method) {
-        ((SpawnFunc)method)(dynWorld, sel, pos, DROPPED_ITEM_ID, 0, 0, nil, nil, 1, 0, nil);
-    }
+    if (method) ((SpawnFunc)method)(dynWorld, sel, pos, DROPPED_ITEM_ID, 0, 0, nil, nil, 1, 0, nil);
 }
 
 void SoftRemoveObject(id obj) {
@@ -62,10 +48,7 @@ void SoftRemoveObject(id obj) {
     if (method) ((VoidFunc)method)(obj, sel, 1);
 }
 
-// --- HOOKS ---
-
 id Hook_Freight_InitPlace(id self, SEL _cmd, id world, id dynWorld, long long pos, id cache, id saveDict, id client) {
-    // Silent block
     SpawnDroppedItem(dynWorld, pos);
     return NULL; 
 }
@@ -75,22 +58,15 @@ id Hook_Freight_InitLoad(id self, SEL _cmd, id world, id dynWorld, id saveDict, 
     if (real_Freight_InitLoad) {
         loadedObj = real_Freight_InitLoad(self, _cmd, world, dynWorld, saveDict, chestDict, cache);
     }
-
     if (loadedObj) {
         long long pos = GetPosition(loadedObj);
-        if (pos != -1) {
-            SpawnDroppedItem(dynWorld, pos);
-        }
+        if (pos != -1) SpawnDroppedItem(dynWorld, pos);
         SoftRemoveObject(loadedObj);
     }
     return loadedObj;
 }
 
-id Hook_Freight_InitNet(id self, SEL _cmd, id world, id dynWorld, id cache, id netData) {
-    return NULL; 
-}
-
-// --- INIT ---
+id Hook_Freight_InitNet(id self, SEL _cmd, id world, id dynWorld, id cache, id netData) { return NULL; }
 
 static void *Freight_InitThread(void *arg) {
     sleep(1); 
@@ -101,24 +77,19 @@ static void *Freight_InitThread(void *arg) {
     if (class_getInstanceMethod(targetClass, selPlace)) {
         method_setImplementation(class_getInstanceMethod(targetClass, selPlace), (IMP)Hook_Freight_InitPlace);
     }
-
     SEL selLoad = sel_registerName(SEL_LOAD);
     Method mLoad = class_getInstanceMethod(targetClass, selLoad);
     if (mLoad) {
         real_Freight_InitLoad = (LoadFunc)method_getImplementation(mLoad);
         method_setImplementation(mLoad, (IMP)Hook_Freight_InitLoad);
     }
-
     SEL selNet = sel_registerName(SEL_NET);
     if (class_getInstanceMethod(targetClass, selNet)) {
         method_setImplementation(class_getInstanceMethod(targetClass, selNet), (IMP)Hook_Freight_InitNet);
     }
-
     return NULL;
 }
 
-__attribute__((constructor))
-static void Freight_Entry() {
-    pthread_t t;
-    pthread_create(&t, NULL, Freight_InitThread, NULL);
+__attribute__((constructor)) static void Freight_Entry() {
+    pthread_t t; pthread_create(&t, NULL, Freight_InitThread, NULL);
 }
