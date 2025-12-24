47de583c8ad5d7f9efd5a5b56086897f7bf0f0d2
FerTheWildShadow <noxthewildshadow@gmail.com>
2025-12-24 15:41:29 -0600

    Update trade_portal_patch.c

--- patches/trade_portal_patch.c
+++ patches/trade_portal_patch.c
@@ -1,7 +1,7 @@
 /*
- * Trade Portal Blocker (FIXED)
- * Target: Trade Portal (ID 210)
- */
+ * Freight Car Ban
+ * Target Class: FreightCar (ID 206)
+ */
 
 #define _GNU_SOURCE
 #include <stdio.h>
@@ -10,182 +10,115 @@
 #include <unistd.h>
 #include <dlfcn.h>
 #include <pthread.h>
-#include <stdbool.h>
 #include <objc/runtime.h>
 #include <objc/message.h>
 
-// Config
-#define TARGET_CLASS "TradePortal" 
-#define BANNED_ID    210            
+// --- Configuration ---
+#define TARGET_CLASS_NAME "FreightCar"
+#define DROPPED_ITEM_ID 206
 
-// Selectors
-#define SEL_PLACE  "initWithWorld:dynamicWorld:atPosition:cache:item:flipped:saveDict:placedByClient:clientName:"
-#define SEL_LOAD   "initWithWorld:dynamicWorld:saveDict:cache:"
-#define SEL_UPDATE "update:accurateDT:isSimulation:" 
-#define SEL_DROP   "destroyItemType"       
-#define SEL_OBJ    "objectType"            
-#define SEL_MACRO  "removeFromMacroBlock"  
-#define SEL_FLAG   "setNeedsRemoved:"      
+// --- Selectors ---
+#define SEL_PLACE "initWithWorld:dynamicWorld:atPosition:cache:saveDict:placedByClient:"
+#define SEL_LOAD "initWithWorld:dynamicWorld:saveDict:chestSaveDict:cache:"
+#define SEL_NET "initWithWorld:dynamicWorld:cache:netData:"
+#define SEL_SPAWN "createFreeBlockAtPosition:ofType:dataA:dataB:subItems:dynamicObjectSaveDict:hovers:playSound:priorityBlockhead:"
+#define SEL_POS "pos"
+#define SEL_FLAG "setNeedsRemoved:"
 
-// Func types
-typedef id (*PlaceFunc)(id, SEL, id, id, long long, id, id, unsigned char, id, id, id);
-typedef id (*LoadFunc)(id, SEL, id, id, id, id);
-typedef void (*UpdateFunc)(id, SEL, float, bool);
-typedef int (*IntFunc)(id, SEL);
-typedef void (*VoidFunc)(id, SEL);
-typedef void (*BoolFunc)(id, SEL, unsigned char);
+// --- Function Prototypes ---
+typedef id (*PlaceFunc)(id, SEL, id, id, long long, id, id, id);
+typedef id (*LoadFunc)(id, SEL, id, id, id, id, id);
+typedef id (*NetFunc)(id, SEL, id, id, id, id);
+typedef long long (*PosReturnFunc)(id, SEL);
+typedef void (*VoidFunc)(id, SEL, BOOL);
+typedef id (*SpawnFunc)(id, SEL, long long, int, int, int, id, id, BOOL, BOOL, id);
 
-static PlaceFunc original_place = NULL;
-static LoadFunc original_load = NULL;
-static UpdateFunc original_update = NULL;
+// --- Global Storage ---
+static LoadFunc real_Freight_InitLoad = NULL;
 
-// -----------------------------------------------------------------------------
-
-int get_item_id(id item) {
-    if (!item) return 0;
-    SEL s = sel_registerName("itemType");
-    Method m = class_getInstanceMethod(object_getClass(item), s);
-    if (m) {
-        int (*f)(id, SEL) = (int (*)(id, SEL))method_getImplementation(m);
-        return f(item, s);
-    }
-    return 0;
+long long GetPosition(id obj) {
+    if (!obj) return -1;
+    SEL sel = sel_registerName(SEL_POS);
+    IMP method = class_getMethodImplementation(object_getClass(obj), sel);
+    if (method) return ((PosReturnFunc)method)(obj, sel);
+    return -1;
 }
 
-int get_deep_id(id block) {
-    if (!block) return 0;
-    Class cls = object_getClass(block);
-    int foundID = 0;
+void SpawnDroppedItem(id dynWorld, long long pos) {
+    if (!dynWorld || pos == -1) return;
 
-    // Try drop type
-    SEL sDrop = sel_registerName(SEL_DROP);
-    if (class_getInstanceMethod(cls, sDrop)) {
-        IntFunc f = (IntFunc)method_getImplementation(class_getInstanceMethod(cls, sDrop));
-        foundID = f(block, sDrop);
-    }
-    // Fallback to object type
-    if (foundID == 0) {
-        SEL sObj = sel_registerName(SEL_OBJ);
-        if (class_getInstanceMethod(cls, sObj)) {
-            IntFunc f = (IntFunc)method_getImplementation(class_getInstanceMethod(cls, sObj));
-            foundID = f(block, sObj);
-        }
-    }
-    return foundID;
+    SEL sel = sel_registerName(SEL_SPAWN);
+    IMP method = class_getMethodImplementation(object_getClass(dynWorld), sel);
+
+    if (method) {
+        ((SpawnFunc)method)(dynWorld, sel, pos, DROPPED_ITEM_ID, 0, 0, nil, nil, 1, 0, nil);
+    }
 }
 
-// -----------------------------------------------------------------------------
-// CLEANUP LOGIC
-
-void safe_remove_fully(id block) {
-    if (!block) return;
-    Class cls = object_getClass(block);
-
-    SEL sMacro = sel_registerName(SEL_MACRO);
-    if (class_getInstanceMethod(cls, sMacro)) {
-        VoidFunc f = (VoidFunc)method_getImplementation(class_getInstanceMethod(cls, sMacro));
-        f(block, sMacro);
-    }
-    SEL sFlag = sel_registerName(SEL_FLAG);
-    if (class_getInstanceMethod(cls, sFlag)) {
-        BoolFunc f = (BoolFunc)method_getImplementation(class_getInstanceMethod(cls, sFlag));
-        f(block, sFlag, 1); 
-    }
+void SoftRemoveObject(id obj) {
+    if (!obj) return;
+    SEL sel = sel_registerName(SEL_FLAG);
+    IMP method = class_getMethodImplementation(object_getClass(obj), sel);
+    if (method) ((VoidFunc)method)(obj, sel, 1);
 }
 
-void mark_for_removal_only(id block) {
-    if (!block) return;
-    Class cls = object_getClass(block);
+// --- HOOKS ---
 
-    SEL sFlag = sel_registerName(SEL_FLAG);
-    if (class_getInstanceMethod(cls, sFlag)) {
-        BoolFunc f = (BoolFunc)method_getImplementation(class_getInstanceMethod(cls, sFlag));
-        f(block, sFlag, 1); 
-    }
+id Hook_Freight_InitPlace(id self, SEL _cmd, id world, id dynWorld, long long pos, id cache, id saveDict, id client) {
+    // Silent block
+    SpawnDroppedItem(dynWorld, pos);
+    return NULL;
 }
 
-// -----------------------------------------------------------------------------
-// HOOKS
+id Hook_Freight_InitLoad(id self, SEL _cmd, id world, id dynWorld, id saveDict, id chestDict, id cache) {
+    id loadedObj = NULL;
+    if (real_Freight_InitLoad) {
+        loadedObj = real_Freight_InitLoad(self, _cmd, world, dynWorld, saveDict, chestDict, cache);
+    }
 
-id hook_TP_Place(id self, SEL _cmd, id world, id dynWorld, long long pos, id cache, id item, unsigned char flipped, id saveDict, id client, id clientName) {
-    
-    if (item) {
-        int id = get_item_id(item);
-        if (id == BANNED_ID) {
-            return NULL;
-        }
-    }
-
-    if (original_place) {
-        return original_place(self, _cmd, world, dynWorld, pos, cache, item, flipped, saveDict, client, clientName);
-    }
-    return NULL;
+    if (loadedObj) {
+        long long pos = GetPosition(loadedObj);
+        if (pos != -1) {
+            SpawnDroppedItem(dynWorld, pos);
+        }
+        SoftRemoveObject(loadedObj);
+    }
+    return loadedObj;
 }
 
-id hook_TP_Load(id self, SEL _cmd, id world, id dynWorld, id saveDict, id cache) {
-    
-    id obj = NULL;
-    if (original_load) {
-        obj = original_load(self, _cmd, world, dynWorld, saveDict, cache);
-    }
-
-    if (obj) {
-        int id = get_deep_id(obj);
-        if (id == BANNED_ID) {
-            mark_for_removal_only(obj);
-            return obj; 
-        }
-    }
-    return obj;
+id Hook_Freight_InitNet(id self, SEL _cmd, id world, id dynWorld, id cache, id netData) {
+    return NULL;
 }
 
-void hook_TP_Update(id self, SEL _cmd, float dt, bool isSim) {
-    
-    int id = get_deep_id(self);
-    
-    if (id == BANNED_ID) {
-        safe_remove_fully(self);
-        return;
-    }
+// --- INIT ---
 
-    if (original_update) {
-        original_update(self, _cmd, dt, isSim);
-    }
-}
+static void *Freight_InitThread(void *arg) {
+    sleep(1);
+    Class targetClass = objc_getClass(TARGET_CLASS_NAME);
+    if (!targetClass) return NULL;
 
-// -----------------------------------------------------------------------------
-static void *TBlocker_InitThread(void *arg) {
-    sleep(2); 
+    SEL selPlace = sel_registerName(SEL_PLACE);
+    if (class_getInstanceMethod(targetClass, selPlace)) {
+        method_setImplementation(class_getInstanceMethod(targetClass, selPlace), (IMP)Hook_Freight_InitPlace);
+    }
 
-    Class targetClass = objc_getClass(TARGET_CLASS);
-    if (!targetClass) return NULL;
+    SEL selLoad = sel_registerName(SEL_LOAD);
+    Method mLoad = class_getInstanceMethod(targetClass, selLoad);
+    if (mLoad) {
+        real_Freight_InitLoad = (LoadFunc)method_getImplementation(mLoad);
+        method_setImplementation(mLoad, (IMP)Hook_Freight_InitLoad);
+    }
 
-    SEL sPlace = sel_registerName(SEL_PLACE);
-    Method mPlace = class_getInstanceMethod(targetClass, sPlace);
-    if (mPlace) {
-        original_place = (PlaceFunc)method_getImplementation(mPlace);
-        method_setImplementation(mPlace, (IMP)hook_TP_Place);
-    }
+    SEL selNet = sel_registerName(SEL_NET);
+    if (class_getInstanceMethod(targetClass, selNet)) {
+        method_setImplementation(class_getInstanceMethod(targetClass, selNet), (IMP)Hook_Freight_InitNet);
+    }
 
-    SEL sLoad = sel_registerName(SEL_LOAD);
-    Method mLoad = class_getInstanceMethod(targetClass, sLoad);
-    if (mLoad) {
-        original_load = (LoadFunc)method_getImplementation(mLoad);
-        method_setImplementation(mLoad, (IMP)hook_TP_Load);
-    }
-
-    SEL sUpdate = sel_registerName(SEL_UPDATE);
-    Method mUpdate = class_getInstanceMethod(targetClass, sUpdate);
-    if (mUpdate) {
-        original_update = (UpdateFunc)method_getImplementation(mUpdate);
-        method_setImplementation(mUpdate, (IMP)hook_TP_Update);
-    }
-
-    return NULL;
+    return NULL;
 }
 
 __attribute__((constructor))
-static void TBlocker_Entry() {
-    pthread_t t;
-    pthread_create(&t, NULL, TBlocker_InitThread, NULL);
+static void Freight_Entry() {
+    pthread_t t;
+    pthread_create(&t, NULL, Freight_InitThread, NULL);
 }
