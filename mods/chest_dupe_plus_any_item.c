ec6b804a4b65f68961295eeb3fc4fd27e6b2cc50
FerTheWildShadow <noxthewildshadow@gmail.com>
2025-12-24 10:47:27 -0600

    Update chest_dupe_plus_any_item.c

--- mods/chest_dupe_plus_any_item.c
+++ mods/chest_dupe_plus_any_item.c
@@ -1,12 +1,3 @@
-/*
- * Chest Dupe + Item/Block Spawner
- * -----------------------------------------------------
- * Commands:
- * /item <ID> <QTY> <PLAYER_NAME> [force]
- * /block <ID> <QTY> <PLAYER_NAME> [force]
- * /dupe <amount>
- */
-
 #define _GNU_SOURCE
 #include <stdio.h>
 #include <stdlib.h>
@@ -15,6 +6,7 @@
 #include <dlfcn.h>
 #include <pthread.h>
 #include <stdbool.h>
+#include <ctype.h>
 #include <objc/runtime.h>
 #include <objc/message.h>
 
@@ -23,23 +15,23 @@
 #define ISP_CHEST_CLASS   "Chest"
 #define ISP_TARGET_ITEM   1043
 
-// --- TYPES (IMPs) ---
+// --- IMP TYPES ---
 typedef id (*ISP_CmdFunc)(id, SEL, id, id);
 typedef void (*ISP_ChatFunc)(id, SEL, id, BOOL, id);
 typedef id (*ISP_PlaceFunc)(id, SEL, id, id, long long, id, id, unsigned char, id, id, id);
 typedef id (*ISP_SpawnFunc)(id, SEL, long long, int, int, int, id, id, BOOL, BOOL, id);
 
-// Memory & Strings
+// Memory & Utils
 typedef id (*ISP_AllocFunc)(id, SEL);
 typedef id (*ISP_InitFunc)(id, SEL);
-typedef void (*ISP_ReleaseFunc)(id, SEL);
-typedef id (*ISP_StrFacFunc)(id, SEL, const char*);
+typedef void (*ISP_VoidFunc)(id, SEL);
+typedef void (*ISP_VoidBoolFunc)(id, SEL, BOOL);
+typedef id (*ISP_StrFunc)(id, SEL, const char*);
 typedef const char* (*ISP_Utf8Func)(id, SEL);
+typedef int (*ISP_IntFunc)(id, SEL);
+typedef id (*ISP_IdxFunc)(id, SEL, int);
 typedef id (*ISP_GetterFunc)(id, SEL);
 typedef long (*ISP_CompFunc)(id, SEL, id);
-typedef int (*ISP_IntFunc)(id, SEL);
-typedef void (*ISP_VoidBoolFunc)(id, SEL, BOOL);
-typedef id (*ISP_IdxFunc)(id, SEL, int);
 
 // --- GLOBALS ---
 static ISP_CmdFunc   Real_ISP_HandleCmd = NULL;
@@ -49,133 +41,94 @@ static ISP_PlaceFunc Real_ISP_ChestPlace = NULL;
 static bool g_ISP_DupeEnabled = false;
 static int  g_ISP_DupeCount = 1;
 
-// --- MEMORY HELPERS (PURE IMP) ---
-
-static id ISP_CreatePool() {
+// --- UTILS ---
+static id ISP_Pool() {
     Class cls = objc_getClass("NSAutoreleasePool");
-    if (!cls) return nil;
+    SEL sA = sel_registerName("alloc");
+    SEL sI = sel_registerName("init");
+    ISP_AllocFunc fA = (ISP_AllocFunc)method_getImplementation(class_getClassMethod(cls, sA));
+    ISP_InitFunc fI = (ISP_InitFunc)method_getImplementation(class_getInstanceMethod(cls, sI));
+    return fI(fA((id)cls, sA), sI);
+}
+
+static void ISP_Drain(id pool) {
+    if (!pool) return;
+    SEL s = sel_registerName("drain");
+    ISP_VoidFunc f = (ISP_VoidFunc)method_getImplementation(class_getInstanceMethod(object_getClass(pool), s));
+    f(pool, s);
+}
+
+static id ISP_Str(const char* txt) {
+    if (!txt) return nil;
+    Class cls = objc_getClass("NSString");
+    SEL s = sel_registerName("stringWithUTF8String:");
+    ISP_StrFunc f = (ISP_StrFunc)method_getImplementation(class_getClassMethod(cls, s));
+    return f ? f((id)cls, s, txt) : nil;
+}
+
+static const char* ISP_CStr(id str) {
+    if (!str) return "";
+    SEL s = sel_registerName("UTF8String");
+    ISP_Utf8Func f = (ISP_Utf8Func)method_getImplementation(class_getInstanceMethod(object_getClass(str), s));
+    return f ? f(str, s) : "";
+}
+
+static void ISP_Chat(id server, const char* msg) {
+    if (server && Real_ISP_SendChat) {
+        Real_ISP_SendChat(server, sel_registerName("sendChatMessage:displayNotification:sendToClients:"), ISP_Str(msg), true, nil);
+    }
+}
+
+// --- LOGIC: FIND PLAYER (ClientName) ---
+id ISP_FindBlockhead(id dynWorld, const char* name) {
+    if (!dynWorld || !name) return nil;
+    Ivar iv = class_getInstanceVariable(object_getClass(dynWorld), "netBlockheads");
+    if (!iv) return nil;
+    id list = *(id*)((char*)dynWorld + ivar_getOffset(iv));
     
-    SEL sAlloc = sel_registerName("alloc");
-    SEL sInit = sel_registerName("init");
+    SEL sCnt = sel_registerName("count");
+    SEL sIdx = sel_registerName("objectAtIndex:");
+    SEL sClientName = sel_registerName("clientName");
+    SEL sComp = sel_registerName("caseInsensitiveCompare:");
     
-    Method mAlloc = class_getClassMethod(cls, sAlloc);
-    Method mInit = class_getInstanceMethod(cls, sInit);
+    ISP_IntFunc fCnt = (ISP_IntFunc)method_getImplementation(class_getInstanceMethod(object_getClass(list), sCnt));
+    ISP_IdxFunc fIdx = (ISP_IdxFunc)method_getImplementation(class_getInstanceMethod(object_getClass(list), sIdx));
     
-    if (mAlloc && mInit) {
-        ISP_AllocFunc fAlloc = (ISP_AllocFunc)method_getImplementation(mAlloc);
-        ISP_InitFunc fInit = (ISP_InitFunc)method_getImplementation(mInit);
-        if (fAlloc && fInit) {
-            return fInit(fAlloc((id)cls, sAlloc), sInit);
+    int count = fCnt(list, sCnt);
+    id target = ISP_Str(name);
+    
+    for (int i=0; i<count; i++) {
+        id pool = ISP_Pool();
+        id bh = fIdx(list, sIdx, i);
+        id cName = nil;
+        
+        Method mName = class_getInstanceMethod(object_getClass(bh), sClientName);
+        if (mName) {
+            ISP_GetterFunc fName = (ISP_GetterFunc)method_getImplementation(mName);
+            cName = fName(bh, sClientName);
+        } else {
+            Ivar ivN = class_getInstanceVariable(object_getClass(bh), "clientName");
+            if (ivN) cName = *(id*)((char*)bh + ivar_getOffset(ivN));
         }
+        
+        if (cName) {
+            ISP_CompFunc fComp = (ISP_CompFunc)method_getImplementation(class_getInstanceMethod(object_getClass(cName), sComp));
+            if (fComp(cName, sComp, target) == 0) {
+                ISP_Drain(pool);
+                return bh;
+            }
+        }
+        ISP_Drain(pool);
     }
     return nil;
 }
 
-static void ISP_ReleasePool(id pool) {
-    if (!pool) return;
-    SEL sRel = sel_registerName("release");
-    Method mRel = class_getInstanceMethod(object_getClass(pool), sRel);
-    if (mRel) {
-        ISP_ReleaseFunc fRel = (ISP_ReleaseFunc)method_getImplementation(mRel);
-        if (fRel) fRel(pool, sRel);
-    }
-}
-
-static id ISP_AllocStr(const char* text) {
-    if (!text) return nil;
-    Class cls = objc_getClass("NSString");
-    SEL sel = sel_registerName("stringWithUTF8String:");
-    Method m = class_getClassMethod(cls, sel);
-    if (!m) return nil;
-    ISP_StrFacFunc f = (ISP_StrFacFunc)method_getImplementation(m);
-    return f ? f((id)cls, sel, text) : nil;
-}
-
-static const char* ISP_GetCStr(id str) {
-    if (!str) return "";
-    SEL sel = sel_registerName("UTF8String");
-    Method m = class_getInstanceMethod(object_getClass(str), sel);
-    if (!m) return "";
-    ISP_Utf8Func f = (ISP_Utf8Func)method_getImplementation(m);
-    return f ? f(str, sel) : "";
-}
-
-static void ISP_SendChat(id server, const char* msg) {
-    if (server && Real_ISP_SendChat) {
-        Real_ISP_SendChat(server, 
-                          sel_registerName("sendChatMessage:displayNotification:sendToClients:"), 
-                          ISP_AllocStr(msg), 
-                          true, 
-                          nil);
-    }
-}
-
-// --- LOGIC: FIND PLAYER (PURE IMP) ---
-id ISP_FindBlockheadForPlayer(id dynWorld, const char* targetName) {
-    if (!dynWorld || !targetName) return nil;
-    
-    Ivar iv = class_getInstanceVariable(object_getClass(dynWorld), "netBlockheads");
-    if (!iv) return nil;
-    
-    id list = *(id*)((char*)dynWorld + ivar_getOffset(iv));
-    if (!list) return nil;
-
-    SEL sCount = sel_registerName("count");
-    SEL sIdx = sel_registerName("objectAtIndex:");
-    SEL sClientName = sel_registerName("clientName"); 
-    SEL sComp = sel_registerName("caseInsensitiveCompare:");
-
-    Method mCount = class_getInstanceMethod(object_getClass(list), sCount);
-    Method mIdx = class_getInstanceMethod(object_getClass(list), sIdx);
-
-    if (!mCount || !mIdx) return nil;
-
-    ISP_IntFunc fCount = (ISP_IntFunc)method_getImplementation(mCount);
-    ISP_IdxFunc fIdx = (ISP_IdxFunc)method_getImplementation(mIdx);
-
-    int count = fCount(list, sCount);
-    id targetStr = ISP_AllocStr(targetName);
-    id foundBH = nil;
-
-    for (int i = 0; i < count; i++) {
-        id pool = ISP_CreatePool();
-        id bh = fIdx(list, sIdx, i);
-        if (bh) {
-            id nsName = nil;
-            // Use property getter for clientName (safer than ivar)
-            Method mName = class_getInstanceMethod(object_getClass(bh), sClientName);
-            if (mName) {
-                ISP_GetterFunc fName = (ISP_GetterFunc)method_getImplementation(mName);
-                nsName = fName(bh, sClientName);
-            } else {
-                Ivar ivN = class_getInstanceVariable(object_getClass(bh), "clientName");
-                if (ivN) nsName = *(id*)((char*)bh + ivar_getOffset(ivN));
-            }
-
-            if (nsName) {
-                Method mComp = class_getInstanceMethod(object_getClass(nsName), sComp);
-                if (mComp) {
-                    ISP_CompFunc fComp = (ISP_CompFunc)method_getImplementation(mComp);
-                    if (fComp(nsName, sComp, targetStr) == 0) {
-                        foundBH = bh; 
-                    }
-                }
-            }
-        }
-        ISP_ReleasePool(pool);
-        if (foundBH) break; 
-    }
-    return foundBH;
-}
-
-// --- LOGIC: FULL BLOCK-TO-ITEM MAP ---
-int ISP_ParseBlockID(int blockID) {
+// --- LOGIC: FULL BLOCK ID LIST ---
+int ISP_ParseID(int blockID) {
     if (blockID > 255) return blockID; 
-    
-    // FULL List restored
     switch (blockID) {
         case 1: return 1024; // Stone
-        case 2: return 0;    // Dirt (Block vs Item diff)
+        case 2: return 0;    // Dirt
         case 3: return 105;  // Water
         case 4: return 1060; // Ice
         case 6: return 1048; // Dirt item
@@ -211,20 +164,14 @@ int ISP_ParseBlockID(int blockID) {
 
 void ISP_Spawn(id dynWorld, id player, int idVal, int qty, id saveDict) {
     if (!player) return;
-    
     Ivar ivP = class_getInstanceVariable(object_getClass(player), "pos");
-    if (!ivP) return;
-    
     long long pos = *(long long*)((char*)player + ivar_getOffset(ivP));
-    if (pos == 0) return;
-
-    SEL sel = sel_registerName("createFreeBlockAtPosition:ofType:dataA:dataB:subItems:dynamicObjectSaveDict:hovers:playSound:priorityBlockhead:");
-    Method mSpawn = class_getInstanceMethod(object_getClass(dynWorld), sel);
-    if (mSpawn) {
-        ISP_SpawnFunc f = (ISP_SpawnFunc)method_getImplementation(mSpawn);
-        for(int i=0; i<qty; i++) {
-            f(dynWorld, sel, pos, idVal, 1, 0, nil, saveDict, 1, 0, player);
-        }
+    
+    SEL sSpawn = sel_registerName("createFreeBlockAtPosition:ofType:dataA:dataB:subItems:dynamicObjectSaveDict:hovers:playSound:priorityBlockhead:");
+    ISP_SpawnFunc fSpawn = (ISP_SpawnFunc)method_getImplementation(class_getInstanceMethod(object_getClass(dynWorld), sSpawn));
+    
+    for(int i=0; i<qty; i++) {
+        fSpawn(dynWorld, sSpawn, pos, idVal, 1, 0, nil, saveDict, 1, 0, player);
     }
 }
 
@@ -235,75 +182,61 @@ id Hook_ISP_ChestPlace(id self, SEL _cmd, id w, id dw, long long pos, id cache, 
     
     if (g_ISP_DupeEnabled && ret && item) {
         SEL sType = sel_registerName("itemType");
-        Method mType = class_getInstanceMethod(object_getClass(item), sType);
-        ISP_IntFunc fType = mType ? (ISP_IntFunc)method_getImplementation(mType) : NULL;
-        int type = fType ? fType(item, sType) : 0;
+        ISP_IntFunc fType = (ISP_IntFunc)method_getImplementation(class_getInstanceMethod(object_getClass(item), sType));
+        int type = fType(item, sType);
 
         if (type == ISP_TARGET_ITEM) {
-            const char* name = ISP_GetCStr(cName);
-            id pool = ISP_CreatePool();
-            
-            // Search by Client Name
-            id player = ISP_FindBlockheadForPlayer(dw, name);
+            const char* name = ISP_CStr(cName);
+            id pool = ISP_Pool();
+            id player = ISP_FindBlockhead(dw, name);
             
             if (player) {
-                // Refund Original (1) + Add Copies (g_ISP_DupeCount)
+                // Spawn Original (1) + Copies (Count)
                 ISP_Spawn(dw, player, ISP_TARGET_ITEM, 1 + g_ISP_DupeCount, save);
                 
-                // Remove placed item immediately
                 SEL sRem = sel_registerName("remove:");
-                Method mRem = class_getInstanceMethod(object_getClass(ret), sRem);
-                if (mRem) {
-                    ISP_VoidBoolFunc fRem = (ISP_VoidBoolFunc)method_getImplementation(mRem);
-                    fRem(ret, sRem, 1);
-                }
+                ISP_VoidBoolFunc fRem = (ISP_VoidBoolFunc)method_getImplementation(class_getInstanceMethod(object_getClass(ret), sRem));
+                fRem(ret, sRem, 1);
             }
-            ISP_ReleasePool(pool);
+            ISP_Drain(pool);
         }
     }
     return ret;
 }
 
 id Hook_ISP_Cmd(id self, SEL _cmd, id cmdStr, id client) {
-    const char* raw = ISP_GetCStr(cmdStr);
-    if (!raw || strlen(raw) == 0) return Real_ISP_HandleCmd(self, _cmd, cmdStr, client);
-
-    id pool = ISP_CreatePool();
-    char buffer[256]; 
-    strncpy(buffer, raw, 255); 
-    buffer[255] = 0;
+    const char* raw = ISP_CStr(cmdStr);
+    if (!raw) return Real_ISP_HandleCmd(self, _cmd, cmdStr, client);
+    
+    id pool = ISP_Pool();
+    char buffer[256]; strncpy(buffer, raw, 255);
     
     bool isItem  = (strncmp(buffer, "/item", 5) == 0);
     bool isBlock = (strncmp(buffer, "/block", 6) == 0);
     bool isDupe  = (strncmp(buffer, "/dupe", 5) == 0);
 
     if (!isItem && !isBlock && !isDupe) {
-        ISP_ReleasePool(pool);
+        ISP_Drain(pool);
         return Real_ISP_HandleCmd(self, _cmd, cmdStr, client);
     }
 
     id world = nil;
-    Ivar ivW = class_getInstanceVariable(object_getClass(self), "world");
-    if (ivW) world = *(id*)((char*)self + ivar_getOffset(ivW));
-
+    object_getInstanceVariable(self, "world", (void**)&world);
     id dynWorld = nil;
-    if (world) {
-        Ivar ivD = class_getInstanceVariable(object_getClass(world), "dynamicWorld");
-        if (ivD) dynWorld = *(id*)((char*)world + ivar_getOffset(ivD));
-    }
+    if (world) object_getInstanceVariable(world, "dynamicWorld", (void**)&dynWorld);
 
     if (!dynWorld) {
-        ISP_SendChat(self, "[Error] World not initialized.");
-        ISP_ReleasePool(pool);
+        ISP_Chat(self, "[Error] World not initialized.");
+        ISP_Drain(pool);
         return nil;
     }
 
+    char *saveptr;
+    strtok_r(buffer, " ", &saveptr);
+    
     // --- DUPE ---
     if (isDupe) {
-        char *saveptr;
-        strtok_r(buffer, " ", &saveptr);
         char* arg1 = strtok_r(NULL, " ", &saveptr);
-        
         if (!arg1) {
             g_ISP_DupeEnabled = !g_ISP_DupeEnabled;
             if (g_ISP_DupeEnabled) g_ISP_DupeCount = 1;
@@ -313,73 +246,68 @@ id Hook_ISP_Cmd(id self, SEL _cmd, id cmdStr, id client) {
             if (g_ISP_DupeCount < 1) g_ISP_DupeCount = 1;
             if (g_ISP_DupeCount > 5) g_ISP_DupeCount = 5;
         }
-
+        
         char msg[128];
-        snprintf(msg, 128, "[Dupe] %s. (Original + %d copies)", g_ISP_DupeEnabled ? "ON" : "OFF", g_ISP_DupeCount);
-        ISP_SendChat(self, msg);
-        ISP_ReleasePool(pool);
+        snprintf(msg, 128, "[Dupe] %s. (Get 1 Original + %d Copies)", g_ISP_DupeEnabled ? "ON" : "OFF", g_ISP_DupeCount);
+        ISP_Chat(self, msg);
+        ISP_Drain(pool);
         return nil;
     }
 
     // --- ITEM/BLOCK ---
-    char *saveptr;
-    strtok_r(buffer, " ", &saveptr);
     char *sID = strtok_r(NULL, " ", &saveptr);
     char *sQty = strtok_r(NULL, " ", &saveptr);
-    char *sPlayer = strtok_r(NULL, " ", &saveptr); 
+    char *sPlayer = strtok_r(NULL, " ", &saveptr);
     char *sForce = strtok_r(NULL, " ", &saveptr);
 
     if (!sID || !sPlayer) {
-        ISP_SendChat(self, "[Usage] /item <ID> <QTY> <CLIENT_NAME> [force]");
-        ISP_ReleasePool(pool);
+        ISP_Chat(self, "[Usage] /item <ID> <QTY> <CLIENT_NAME> [force]");
+        ISP_Drain(pool);
         return nil;
     }
 
-    // Find Player
-    id targetBH = ISP_FindBlockheadForPlayer(dynWorld, sPlayer);
-    
+    id targetBH = ISP_FindBlockhead(dynWorld, sPlayer);
     if (!targetBH) {
         char err[128];
         snprintf(err, 128, "[Error] Client '%s' not found.", sPlayer);
-        ISP_SendChat(self, err);
-        ISP_ReleasePool(pool);
+        ISP_Chat(self, err);
+        ISP_Drain(pool);
         return nil;
     }
 
     int qty = sQty ? atoi(sQty) : 1;
     if (qty < 1) qty = 1;
-    bool force = (sForce && strcasecmp(sForce, "force") == 0);
     
+    bool force = (sForce && strcasecmp(sForce, "force") == 0);
     if (!force && qty > 99) {
         qty = 99;
-        ISP_SendChat(self, "[Warn] Capped at 99. Use 'force' to override.");
+        ISP_Chat(self, "[Warn] Capped at 99. Use 'force' to override.");
     }
 
     int itemID = atoi(sID);
-    if (isBlock) itemID = ISP_ParseBlockID(itemID);
+    if (isBlock) itemID = ISP_ParseID(itemID); // Convert Block->Item ID
     
     ISP_Spawn(dynWorld, targetBH, itemID, qty, nil);
     
     char successMsg[128];
     snprintf(successMsg, 128, "[System] Gave %d x (ID: %d) to %s.", qty, itemID, sPlayer);
-    ISP_SendChat(self, successMsg);
+    ISP_Chat(self, successMsg);
 
-    ISP_ReleasePool(pool);
+    ISP_Drain(pool);
     return nil;
 }
 
-static void* ISP_InitThread(void* arg) {
+// --- INIT ---
+static void* ISP_Init(void* arg) {
     sleep(1);
     Class clsServer = objc_getClass(ISP_SERVER_CLASS);
     if (clsServer) {
         Method mC = class_getInstanceMethod(clsServer, sel_registerName("handleCommand:issueClient:"));
         Real_ISP_HandleCmd = (ISP_CmdFunc)method_getImplementation(mC);
         method_setImplementation(mC, (IMP)Hook_ISP_Cmd);
-        
         Method mT = class_getInstanceMethod(clsServer, sel_registerName("sendChatMessage:displayNotification:sendToClients:"));
         Real_ISP_SendChat = (ISP_ChatFunc)method_getImplementation(mT);
     }
-
     Class clsChest = objc_getClass(ISP_CHEST_CLASS);
     if (clsChest) {
         Method mP = class_getInstanceMethod(clsChest, sel_registerName("initWithWorld:dynamicWorld:atPosition:cache:item:flipped:saveDict:placedByClient:clientName:"));
@@ -390,6 +318,5 @@ static void* ISP_InitThread(void* arg) {
 }
 
 __attribute__((constructor)) static void ISP_Entry() {
-    pthread_t t; 
-    pthread_create(&t, NULL, ISP_InitThread, NULL);
+    pthread_t t; pthread_create(&t, NULL, ISP_Init, NULL);
 }
